abd1af4444eafff7addf1177202fb7ac7391844b
JohanFernandoLema <johan.13fernando@gmail.com>
2023-06-27 11:50:32 -0400

    input box accepts any city and retrieves its info

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 // console.log(fetch(url))
-const btn = document.querySelector('.weatherBtn')
-const cityName = document.querySelector('.city')
-const some = document.querySelector('.something')
 
-// const url =
+const city = document.querySelector('.cityInput')
+const searchCity = document.querySelector('.weatherBtn')
 
-btn.addEventListener('click', () => {
-  fetch(
-    'http://api.openweathermap.org/data/2.5/forecast?q=' +
-      cityName.value +
-      '&appid=15123b3ed925926863dab6d6946847bb'
+// const url
+async function getWeather() {
+  await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=15123b3ed925926863dab6d6946847bb`
   )
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      // Get weather info
+      document.querySelector('.city').innerHTML = data.city.name
+      document.querySelector('.temp').innerHTML = data.list[0].main.temp
+      document.querySelector('.wind').innerHTML = data.list[0].wind.speed
+    })
     .catch((err) => console.log(err))
+}
+
+searchCity.addEventListener('click', () => {
+  getWeather()
 })
