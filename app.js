4090b88b20b296eceb7d9aa21af8ccdda22ed856
JohanFernandoLema <johan.13fernando@gmail.com>
2023-06-27 15:19:03 -0400

    weather images added to our js file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const city = document.querySelector('.cityInput')
 const searchCity = document.querySelector('.weatherBtn')
+const weatherIcon = document.querySelector('.weather-icon')
 
 // const url
 async function getWeather() {
@@ -19,6 +20,20 @@ async function getWeather() {
         data.list[0].wind.speed + ' Km/h'
       document.querySelector('.humidity').innerHTML =
         data.list[0].main.humidity + ' %'
+
+      if (data.weather[0].main == 'Clouds') {
+        weatherIcon.src = './Assets/images/clouds.png'
+      } else if (data.weather[0].main == 'Clear') {
+        weatherIcon.src = './Assets/images/clear.png'
+      } else if (data.weather[0].main == 'clouds') {
+        weatherIcon.src = './Assets/images/clouds.png'
+      } else if (data.weather[0].main == 'Rain') {
+        weatherIcon.src = './Assets/images/rain.png'
+      } else if (data.weather[0].main == 'Drizzle') {
+        weatherIcon.src = './Assets/images/drizzle.png'
+      } else if (data.weather[0].main == 'Mist') {
+        weatherIcon.src = './Assets/images/mist.png'
+      }
     })
     .catch((err) => console.log(err))
 }
